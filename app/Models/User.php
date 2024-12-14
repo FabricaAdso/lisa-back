4b6513cb1a5701de15a5a982817847cd4897bc08
Anttoniio11 <anttoniio2312@gmail.com>
2024-12-14 15:33:06 -0500

    Modificacion en la funcion me - traer roles en un arreglo

--- app/Models/User.php
+++ app/Models/User.php
@@ -98,7 +98,8 @@ class User extends Authenticatable implements JWTSubject
                 'regional_id' => $center->first()->regional_id,
                 'roles' => $center->pluck('pivot.role_id')->unique()->map(function($roleId) {
                      return \Spatie\Permission\Models\Role::findById($roleId)->name;
-                })->implode(', '),
+                // })->implode(', '),
+                })->all(),
             ];
         });
 

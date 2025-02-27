f818b627f5b480e89986d9de4f3cb4d8572a4c58
Gabriela Jineth Diaz Gomez -2774315 <Gabyd06910@gmail.com>
2025-02-27 11:24:40 -0500

    Detalle en el RapController

--- routes/api.php
+++ routes/api.php
@@ -121,6 +121,7 @@ Route::group(['middleware' => 'auth:api'], function () {
     Route::resource('rap', RapController::class);
     //session
     Route::post('session', [SessionController::class, 'createSession']);
+    Route::get('session', [SessionController::class, 'index']);
     Route::put('session/update/{sessionIds}', [SessionController::class, 'updateSessions']);
     Route::delete('session/{id}', [SessionController::class, 'destroy']);
     // Route::resource('sessions', SessionController::class);
@@ -135,7 +136,6 @@ Route::group(['middleware' => 'auth:api'], function () {
 });
 Route::post('excel', [ExcelController::class, 'excel']);
 
-Route::get('session', [SessionController::class, 'index']);
 // Ruta instructor & Apprentice
 Route::resource('instructor', InstructorController::class);
 //Ruta regionales

2fcfefc20cc64d446e0f5eefbdcc48d299309c11
AntonioRodriguez-2774315 <anttoniio2312@gmail.com>
2025-03-17 08:53:42 -0500

    Se mofifica la fincion assignRoles con el fin de agregar el usuario a su respectiva tabla segun el rol asignado

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -24,56 +24,15 @@ class RoleController extends Controller
         return $roles;
     }
 
-    // public function assignRoles(Request $request)
-    // {
-    //     $request->validate([
-    //         'user_id' => 'required|exists:users,id',
-    //         'role_ids' => 'required|array',
-    //         'role_ids.*' => 'exists:roles,id',
-    //     ]);
-
-    //     $user = User::findOrFail($request->user_id);
-    //     $trainingCenterId = $this->token_service->getTrainingCenterIdFromToken();
-
-    //     try {
-    //         DB::beginTransaction();
-    //         DB::table('role_training_center_user')
-    //             ->where('user_id', $user->id)
-    //             ->where('training_center_id', $trainingCenterId)
-    //             ->delete();
-
-    //         $roles = Role::whereIn('id', $request->role_ids)->get();
-
-    //         $roleData = $roles->map(function ($role) use ($user, $trainingCenterId) {
-    //             return [
-    //                 'user_id' => $user->id,
-    //                 'role_id' => $role->id,
-    //                 'training_center_id' => $trainingCenterId,
-    //                 'created_at' => now(),
-    //                 'updated_at' => now(),
-    //             ];
-    //         })->toArray();
-
-    //         DB::table('role_training_center_user')->insert($roleData);
-    //         DB::commit();
-
-    //         return response()->json([
-    //             'message' => 'Roles asignados exitosamente.',
-    //             'user' => $user,
-    //             'roles' => $roles
-    //         ], 200);
-    //     } catch (Exception $e) {
-    //         DB::rollBack();
-    //         return response()->json(['error' => $e->getMessage()], 400);
-    //     }
-    // }
-
     public function assignRoles(Request $request)
     {
         $request->validate([
             'user_id' => 'required|exists:users,id',
             'role_ids' => 'required|array',
             'role_ids.*' => 'exists:roles,id',
+            'course_id' => 'nullable|exists:courses,id',
+            'state' => 'nullable|in:Formacion,Desertado,Etapa_productiva,Retiro_voluntario',
+            'knowledge_network_id' => 'nullable|exists:knowledge_networks,id',
         ]);
 
         $user = User::findOrFail($request->user_id);
@@ -82,16 +41,13 @@ class RoleController extends Controller
         try {
             DB::beginTransaction();
 
-            // Eliminar roles existentes para el usuario en el centro de entrenamiento
             DB::table('role_training_center_user')
                 ->where('user_id', $user->id)
                 ->where('training_center_id', $trainingCenterId)
                 ->delete();
 
-            // Obtener los roles que se van a asignar
             $roles = Role::whereIn('id', $request->role_ids)->get();
 
-            // Preparar los datos para insertar en la tabla pivote
             $roleData = $roles->map(function ($role) use ($user, $trainingCenterId) {
                 return [
                     'user_id' => $user->id,
@@ -102,19 +58,26 @@ class RoleController extends Controller
                 ];
             })->toArray();
 
-            // Insertar los roles en la tabla pivote
             DB::table('role_training_center_user')->insert($roleData);
 
             // Verificar si el usuario tiene el rol de "aprendiz" o "instructor"
-            $isApprentice = $roles->contains('name', 'aprendiz');
-            $isInstructor = $roles->contains('name', 'instructor');
+            $isApprentice = $roles->contains('name', 'Aprendiz');
+            $isInstructor = $roles->contains('name', 'Instructor');
 
             // Si tiene el rol de "aprendiz", agregarlo a la tabla `apprentices`
             if ($isApprentice) {
+                if (!$request->has('course_id')){
+                    throw new \Exception('El campo course_id es requerido para el rol de aprendiz.');
+                }
+                if (!$request->has('state')) {
+                    throw new \Exception('El campo state es requerido para el rol de aprendiz.');
+                }
+
                 DB::table('apprentices')->updateOrInsert(
                     ['user_id' => $user->id],
                     [
-                        'training_center_id' => $trainingCenterId,
+                        'course_id' => $request->course_id,
+                        'state' => $request->state,
                         'created_at' => now(),
                         'updated_at' => now(),
                     ]
@@ -126,10 +89,16 @@ class RoleController extends Controller
 
             // Si tiene el rol de "instructor", agregarlo a la tabla `instructors`
             if ($isInstructor) {
+                if (!$request->has('knowledge_network_id')) {
+                    throw new \Exception('El campo knowledge_network_id es requerido para el rol de instructor.');
+                }
+
                 DB::table('instructors')->updateOrInsert(
                     ['user_id' => $user->id],
                     [
                         'training_center_id' => $trainingCenterId,
+                        'knowledge_network_id' => $request->knowledge_network_id,
+                        'state' => 'Activo',
                         'created_at' => now(),
                         'updated_at' => now(),
                     ]
@@ -146,7 +115,7 @@ class RoleController extends Controller
                 'user' => $user,
                 'roles' => $roles
             ], 200);
-        } catch (Exception $e) {
+        } catch (\Exception $e) {
             DB::rollBack();
             return response()->json(['error' => $e->getMessage()], 400);
         }

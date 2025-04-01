bdd828a12fc6b3658ddf57a3450ad2cbf2093729
Gabriela Jineth Diaz Gomez -2774315 <Gabyd06910@gmail.com>
2025-04-01 10:57:52 -0500

    Implemetacion del servicio GoogleCalendar en el createSession

--- app/Services/Implementations/SessionServiceImpl.php
+++ app/Services/Implementations/SessionServiceImpl.php
@@ -10,6 +10,7 @@ use App\Models\Rap;
 use App\Models\Session;
 use App\Models\Subject;
 use App\Models\User;
+use App\Services\GoogleCalendarService;
 use App\Services\SessionService;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
@@ -18,6 +19,13 @@ use Illuminate\Support\Facades\Auth;
 class SessionServiceImpl implements SessionService
 
 {
+    protected $googleCalendarService;
+
+    public function __construct(GoogleCalendarService $googleCalendarService)
+    {
+        $this->googleCalendarService = $googleCalendarService;
+    }
+
     public function createSession(Request $request)
     {
 
@@ -39,6 +47,7 @@ class SessionServiceImpl implements SessionService
         if ($isValid != null) {
             return $isValid;
         }
+
         // registro de cambio del porcentaje por usuario
         $rapForUser = Rap::find($request->rap_id);
         $subject = Subject::find($rapForUser->subject_id);
@@ -48,28 +57,14 @@ class SessionServiceImpl implements SessionService
             'percentage' => $request->percentage,
             'updated_porcentage' => Carbon::now()
         ]);
-
-
-        $festivos = [
-            '2025-01-01',
-            '2025-01-06',
-            '2025-03-24',
-            '2025-04-17',
-            '2025-04-18',
-            '2025-05-01',
-            '2025-06-02',
-            '2025-06-23',
-            '2025-06-30',
-            '2025-07-20',
-            '2025-08-07',
-            '2025-08-18',
-            '2025-10-13',
-            '2025-11-03',
-            '2025-11-17',
-            '2025-12-08',
-            '2025-12-25'
-        ];
-
+        
+        $festivos = array_map(function ($holiday) {
+            return $holiday['start']['date']; // Extrae solo la fecha de inicio
+        }, $this->googleCalendarService->getHolidays(date('Y')));
+        
+        // return response()->json($festivos);
+        
+        
         // Obtener la duración total de la competencia en horas
         $rap = Rap::findOrFail($request->rap_id);
         $totalHours = $rap->number_hours;

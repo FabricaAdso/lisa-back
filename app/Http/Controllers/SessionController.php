cb9d7fdff0b7d5e1b8a6498477509a63ee4a5f28
r3stodev <restodev@proton.me>
2025-04-01 15:43:57 -0500

    debounce en submit edit y create

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -163,6 +163,8 @@ class SessionController extends Controller
 
         $sessions = Session::leaderFilter($filters, $courseIds)
             ->select('*', DB::raw("DATE_FORMAT(start_time, '%H:%i') as start_time"), DB::raw("DATE_FORMAT(end_time, '%H:%i') as end_time"))
+            ->orderBy('date', 'ASC')
+
             ->included()
             ->paginate(intval($elements), ['*'], 'page', $page);
 

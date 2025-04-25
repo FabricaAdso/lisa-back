5b9272fc8c08a46fe5199d079c750549de096fb3
Camilo Maca - ficha:2774315 <camilomaca741@gmail.com>
2025-04-25 12:04:14 -0500

    datos faltantes completados para calendario

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -91,6 +91,7 @@ class SessionController extends Controller
                 'rap_id' => $session->rap_id,
                 'instructor2_id' => $session->instructor2_id,
                 'course_id' => $session->course_id,
+                'environment' => $session->course->environment->name,
                 'headquarters' => optional(
                                 optional($session->course->environment)->headquarters
                              )->name,
